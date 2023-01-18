9581be2b899540537b7df62db5d1da4aaefa2b01
pszempszemek <jestempszemek@gmail.com>
2023-01-18 23:44:09 +0100

    Remove: Component2

--- src/components/Component1.jsx
+++ src/components/Component1.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Component2 from './Component2'
+import Component3 from './Component3'
 
 
 export default function Component1(){
   return (
   <div>
     <h2>Component1</h2>
-    <Component2 />
+    <Component3 />
   </div>
   )
 }
