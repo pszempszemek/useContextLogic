e8de24edfeb2367d2a3490586e676b3f09fac64b
pszempszemek <jestempszemek@gmail.com>
2023-01-19 00:43:07 +0100

    Refactor: Component5

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import React, { useState, useContext } from "react";
+import React, { useState, createContext } from "react";
 import Component1 from "./components/Component1";
-import { NameContext } from "./NameContext";
 
+// bez sensu takiego jednolinijkowca exportować do osobnego pliku
+export const NameContext = createContext("Starting name");
 
 export default function App() {
-  const [name, setName] = useState('Andrzej')
+  const [name, setName] = useState("Andrzej");
 
   return (
-    <NameContext.Provider value={{name, setName}}>
+    <NameContext.Provider value={{ name, setName }}>
       <main>
-          <h1>hello {name}</h1>
-          <Component1 />
+        <h1>hello {name}</h1>
+        <Component1 />
       </main>
     </NameContext.Provider>
   );
--- src/components/Component1.jsx
+++ src/components/Component1.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Component3 from './Component3'
+import Component2 from './Component2'
 
 
 export default function Component1(){
   return (
   <div>
     <h2>Component1</h2>
-    <Component3 />
+    <Component2 />
   </div>
   )
 }
