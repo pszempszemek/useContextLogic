0da77cc705cb78cda1c444df049a9132e2e87004
pszempszemek <jestempszemek@gmail.com>
2023-01-19 00:09:42 +0100

    Modify from master: Component4

--- src/components/Component4.jsx
+++ src/components/Component4.jsx
@@ -5,7 +5,7 @@ import Component5 from './Component5'
 export default function Component4(){
   return (
   <div>
-    <h2>Component4</h2>
+    <h2>Component4 Modified from master</h2>
     <Component5 />
   </div>
   )
